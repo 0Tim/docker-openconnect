3bfed4726da922f50cd654b1c3e0ae68744e1424
0Tim <tmieulet@gmail.com>
2016-04-03 10:02:49 +0200

    open VPN automatically

--- openconnect/exe.sh
+++ openconnect/exe.sh
@@ -3,7 +3,9 @@
 echo "Starting openconect"
 echo "${VPN_PASSWORD}" | openconnect --background --pid-file=/run/openconnect_vpn.pid ${VPN_ARGS}
 
+
 # USE the trap if you need to also do manual cleanup after the service is stopped,
 #     or need to start multiple services in the one container
 trap "kill -INT $(cat /run/openconnect_vpn.pid) ;" HUP INT QUIT KILL TERM
 
+exec "/bin/bash"
